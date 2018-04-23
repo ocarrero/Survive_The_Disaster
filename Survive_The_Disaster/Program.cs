25ada55fa14db0c231e3d88ffa6a2d21954925f6
Oscar Carrero <oscar.carrero@outlook.com>
2018-04-23 11:49:58 -0500

    // This one line is for the sole purpose of making sure

--- Survive_The_Disaster/Program.cs
+++ Survive_The_Disaster/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace Survive_The_Disaster
 
 {
-    // This one line is for the sole purpose of making sur
+    // This one line is for the sole purpose of making sure
     public class Program
     {
         public static void Main(string[] args)
