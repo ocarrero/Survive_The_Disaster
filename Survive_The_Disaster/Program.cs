5f3269c67b9ab332c49fa28e6e83c6a17f093f5a
Oscar Carrero <oscar.carrero@outlook.com>
2018-04-23 11:44:06 -0500

    // This one line is for the sole purpose of ma

--- Survive_The_Disaster/Program.cs
+++ Survive_The_Disaster/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace Survive_The_Disaster
 
 {
-    // This one line is for the sole purpose of m
+    // This one line is for the sole purpose of ma
     public class Program
     {
         public static void Main(string[] args)
