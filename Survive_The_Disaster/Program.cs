309b3c7b45602016e18b540ef3174fad2fe8f817
Oscar Carrero <oscar.carrero@outlook.com>
2018-04-23 11:35:01 -0500

    // This one line is for the s

--- Survive_The_Disaster/Program.cs
+++ Survive_The_Disaster/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace Survive_The_Disaster
 
 {
-    // This one line is for the 
+    // This one line is for the s
     public class Program
     {
         public static void Main(string[] args)
