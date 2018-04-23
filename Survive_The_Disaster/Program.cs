806eb6d10ad2e51beb2478e8c9718046f6a5e6e7
Oscar Carrero <oscar.carrero@outlook.com>
2018-04-23 11:40:20 -0500

    // This one line is for the sole purpo

--- Survive_The_Disaster/Program.cs
+++ Survive_The_Disaster/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace Survive_The_Disaster
 
 {
-    // This one line is for the sole purp
+    // This one line is for the sole purpo
     public class Program
     {
         public static void Main(string[] args)
