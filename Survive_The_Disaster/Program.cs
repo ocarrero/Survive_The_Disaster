728d0fe0da78de87e4e354c4a62bfe105e1b77e3
Oscar Carrero <oscar.carrero@outlook.com>
2018-04-23 12:08:36 -0500

    // This one line is for the sole purpose of making sure I hit one hundred commits a

--- Survive_The_Disaster/Program.cs
+++ Survive_The_Disaster/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace Survive_The_Disaster
 
 {
-    // This one line is for the sole purpose of making sure I hit one hundred commits 
+    // This one line is for the sole purpose of making sure I hit one hundred commits a
     public class Program
     {
         public static void Main(string[] args)
