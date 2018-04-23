81156cca3c5df50e14d69fb93ed9237a6c6d7540
Oscar Carrero <oscar.carrero@outlook.com>
2018-04-23 11:14:23 -0500

    Add project files.

---
+++ Survive_The_Disaster/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Survive_The_Disaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Survive_The_Disaster/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Survive_The_Disaster.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
