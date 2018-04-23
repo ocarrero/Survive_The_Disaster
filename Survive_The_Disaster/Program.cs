7f0596ede0dfc4fcda16a592daa084c48eee1e54
Oscar Carrero <oscar.carrero@outlook.com>
2018-04-23 11:37:00 -0500

    // This one line is for the sole

--- Survive_The_Disaster/Program.cs
+++ Survive_The_Disaster/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace Survive_The_Disaster
 
 {
-    // This one line is for the sol
+    // This one line is for the sole
     public class Program
     {
         public static void Main(string[] args)
